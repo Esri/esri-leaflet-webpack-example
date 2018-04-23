e0e0273d94ede303de6af0078bd65c8510acdc0d
ajzeigert <ajzeigert@gmail.com>
2018-04-23 13:47:50 -0700

    changes zoom and stuff

--- index.js
+++ index.js
@@ -2,27 +2,20 @@ require([
       "esri/Map",
       "esri/views/MapView",
       "esri/geometry/Extent",
-      "esri/geometry/SpatialReference",
+      // "esri/geometry/SpatialReference",
       "dojo/domReady!"
-    ], function(Map, MapView, Extent, SpatialReference) {
+    ], function(Map, MapView, Extent) {
 
       var map = new Map({
         basemap: "streets",
         ground: "world-elevation"
       });
 
-      var ext = new Extent({
-          xmin: -13056650,
-          ymin: 6077558,
-          xmax: -13055709,
-          ymax: 6077938,
-          spatialReference: new SpatialReference({wkid:3857})
-      });
-
       var view = new MapView({
         container: "viewDiv",
         map: map,
-        extent: ext
-        
+        // extent: ext
+        center: [-100, 40],
+		zoom: 4
       });
 });
